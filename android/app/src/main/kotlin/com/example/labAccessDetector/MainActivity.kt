27939cf5f486b13d7032e1b26b4277f3b86b47d9
sssht03 <matsuo.shuta@arakawa-lab.com>
2021-02-14 04:36:15 +0900

    change bundle id

--- android/app/src/main/kotlin/com/example/labAccessDetector/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.labAccessDetector
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/imago/iqlablog/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.imago.iqlablog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {}
